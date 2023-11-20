ca7d3989d42feeadeff926606be14931f57ffe7c
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-11-20 11:46:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+    let keys = Object.keys(sampleObject);
+  // console.log(keys.indexOf(key));
+  if (keys.indexOf(key) !== -1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Do not change the code below
